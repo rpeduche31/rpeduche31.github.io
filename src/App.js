da25b12e35939f6612e682e5bf239a8714586a0d
Rochester Peduche <r.peduche@samsung.com>
2022-09-13 16:45:55 +0800

    with adsense

--- src/App.js
+++ src/App.js
@@ -1,48 +1,35 @@
 import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Helmet} from 'react-helmet';
 
 function App() {
-  const NewComponent = ({slot}) => {
-    useEffect(() => {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    });
+  // useEffect(() => {
+  //   (window.adsbygoogle = window.adsbygoogle || []).push({});
+  // });
 
-    return (
-      <>
+  return (
+    <div className='App'>
+      <Helmet>
         <script
           async
           src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2816831416359590'
-          crossorigin='anonymous'
+          crossOrigin='anonymous'
         ></script>
         <ins
-          class='adsbygoogle'
+          className='adsbygoogle'
           style={{display: 'block'}}
           data-ad-client='ca-pub-2816831416359590'
           data-ad-slot='2625787741'
           data-ad-format='auto'
           data-full-width-responsive='true'
         ></ins>
-        <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-      </>
-    );
-  };
+        {/* <script>{`(adsbygoogle = window.adsbygoogle || []).push({})`}</script> */}
+      </Helmet>
 
-  const ads = (
-    <script
-      async
-      src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2816831416359590'
-      crossorigin='anonymous'
-    ></script>
-  );
-
-  return (
-    <div className='App'>
       <header className='App-header'>
-        {ads && ads}
         <img src={logo} className='App-logo' alt='logo' />
         TEST
-        <NewComponent />
       </header>
     </div>
   );
